3888846a5c0e81ec39faff59228e4f92c594fcd2
Wolframfriele <wolframfriele@gmail.com>
2022-05-11 12:39:05 +0200

    Try editing the base path

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const url = dev ? '/' : 'wolf'; // <<-
+const url = dev ? '/' : '/'; // <<-
 
 polka() // You can also use Express
 	.use(
