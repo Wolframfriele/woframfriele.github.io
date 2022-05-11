8d9a68493f54aefdb1fcb4e7343e8e299dfb022f
Wolframfriele <wolframfriele@gmail.com>
2022-05-11 11:47:39 +0200

    Try to fix server URL error

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const url = dev ? '/' : 'wolframfriele.github.io'; // <<-
+const url = dev ? '/' : '/'; // <<-
 
 polka() // You can also use Express
 	.use(
